bb502dc4fabc48126d1d43862ca9185a0fb81d0a
chlalswns200 <chlalswns200@naver.com>
2022-10-04 15:34:23 +0900

    수업시간 문제

--- src/day10/OddEvenAndPlus.java
+++ src/day10/OddEvenAndPlus.java
@@ -9,19 +9,15 @@ public class OddEvenAndPlus {
         int numA = sc.nextInt();
         int numB = sc.nextInt();
 
+        OddEven(numA);
+        System.out.print("+");
+        OddEven(numB);
+        System.out.print("=");
+        OddEven(numA+numB);
+    }
+
+    private static void OddEven(int numA) {
         if (numA % 2 == 0) {
-            System.out.print("짝수+");
-        } else {
-            System.out.print("홀수+");
-        }
-
-        if (numB % 2 == 0) {
-            System.out.print("짝수=");
-        } else {
-            System.out.print("홀수=");
-        }
-
-        if ((numA + numB) % 2 == 0) {
             System.out.print("짝수");
         } else {
             System.out.print("홀수");
