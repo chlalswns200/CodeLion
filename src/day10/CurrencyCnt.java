6a49439aff2edb7360e8b03795f59126edc558da
chlalswns200 <chlalswns200@naver.com>
2022-10-04 13:50:38 +0900

    수업시간 문제

--- src/day10/CurrencyCnt.java
+++ src/day10/CurrencyCnt.java
@@ -10,88 +10,11 @@ public class CurrencyCnt {
         System.out.print("금액을 넣으세요: ");
         amount = sc.nextInt();
 
-        while (true) {
+        int[] currency = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10};
+        for (int i : currency) {
+            System.out.println(i+ "=" +amount / i);
+            amount %= i;
 
-            if (amount - 50000 >= 0) {
-                amount -= 50000;
-                a += 1;
-            } else {
-                break;
-            }
         }
-        System.out.println("50000원" + a + "개");
-        while (true) {
-
-            if (amount - 10000 >= 0) {
-                amount -= 10000;
-                b += 1;
-            } else {
-                break;
-            }
-        }
-        System.out.println("10000원" + b + "개");
-        while (true) {
-
-            if (amount - 5000 >= 0) {
-                amount -= 5000;
-                c += 1;
-            } else {
-                break;
-            }
-        }
-        System.out.println("5000원" + c + "개");
-        while (true) {
-
-            if (amount - 1000 >= 0) {
-                amount -= 1000;
-                d += 1;
-            } else {
-                break;
-            }
-        }
-        System.out.println("1000원" + d + "개");
-        while (true) {
-
-            if (amount - 500 >= 0) {
-                amount -= 500;
-                e += 1;
-            } else {
-                break;
-            }
-        }
-        System.out.println("500원" + e + "개");
-
-        while (true) {
-
-            if (amount - 100 >= 0) {
-                amount -= 100;
-                f += 1;
-            } else {
-                break;
-            }
-        }
-        System.out.println("100원" + f + "개");
-
-        while (true) {
-
-            if (amount - 50 >= 0) {
-                amount -= 50;
-                g += 1;
-            } else {
-                break;
-            }
-        }
-        System.out.println("50원" + g + "개");
-
-        while (true) {
-
-            if (amount - 10 >= 0) {
-                amount -= 10;
-                h += 1;
-            } else {
-                break;
-            }
-        }
-        System.out.println("10원" + h + "개");
     }
 }
