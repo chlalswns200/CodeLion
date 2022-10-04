c1f2c61d4b8bdaded421494f17f90f2e65eb6c2c
chlalswns200 <chlalswns200@naver.com>
2022-10-04 14:44:40 +0900

    수업시간 문제

--- src/day10/MoneyCount2.java
+++ src/day10/MoneyCount2.java
@@ -8,8 +8,7 @@ public class MoneyCount2 {
         Scanner scan = new Scanner(System.in);
         int[] num ={50000,10000,5000,1000,500,100,50,10};
 
-        System.out.print("금액을 넣으세요 : ");
-        int money = scan.nextInt();
+        int refund = 25000;
 
         int curr50000 = 50000;
         int curr10000 = 10000;
@@ -20,35 +19,35 @@ public class MoneyCount2 {
         int curr50 = 50;
         int curr10 = 10;
 
-        System.out.println(curr50000+"원 : "+money/curr50000);
-        money = money%curr50000;
-        System.out.println("나머지 금액 :" +money);
+        System.out.print(curr50000+"원 : "+refund/curr50000);
+        refund = refund%curr50000;
+        System.out.println(" 나머지 금액 :" +refund);
 
-        System.out.println(curr10000+"원 : "+money/curr10000);
-        money = money%curr10000;
-        System.out.println("나머지 금액 :" +money);
+        System.out.print(curr10000+"원 : "+refund/curr10000);
+        refund = refund%curr10000;
+        System.out.println(" 나머지 금액 :" +refund);
 
-        System.out.println(curr5000+"원 : "+money/curr5000);
-        money = money%curr5000;
-        System.out.println("나머지 금액 :" +money);
+        System.out.print(curr5000+"원 : "+refund/curr5000);
+        refund = refund%curr5000;
+        System.out.println(" 나머지 금액 :" +refund);
 
-        System.out.println(curr1000+"원 : "+money/curr1000);
-        money = money%curr1000;
-        System.out.println("나머지 금액 :" +money);
+        System.out.print(curr1000+"원 : "+refund/curr1000);
+        refund = refund%curr1000;
+        System.out.println(" 나머지 금액 :" +refund);
 
-        System.out.println(curr500+"원 : "+money/curr500);
-        money = money%curr500;
-        System.out.println("나머지 금액 :" +money);
+        System.out.print(curr500+"원 : "+refund/curr500);
+        refund = refund%curr500;
+        System.out.println(" 나머지 금액 :" +refund);
 
-        System.out.println(curr100+"원 : "+money/curr100);
-        money = money%curr100;
-        System.out.println("나머지 금액 :" +money);
+        System.out.print(curr100+"원 : "+refund/curr100);
+        refund = refund%curr100;
+        System.out.println(" 나머지 금액 :" +refund);
 
-        System.out.println(curr50+"원 : "+money/curr50);
-        money = money%curr50;
-        System.out.println("나머지 금액 :" +money);
+        System.out.print(curr50+"원 : "+refund/curr50);
+        refund = refund%curr50;
+        System.out.println(" 나머지 금액 :" +refund);
 
-        System.out.println(curr10+"원 : "+money/curr10);
+        System.out.print(curr10+"원 : "+refund/curr10);
 
     }
 }
