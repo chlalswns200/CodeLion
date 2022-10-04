cc19a04ebeb86a4fb59e126357baa3db52c00d10
chlalswns200 <chlalswns200@naver.com>
2022-10-04 14:33:09 +0900

    수업시간 문제

--- src/day10/MoneyCount.java
+++ src/day10/MoneyCount.java
@@ -4,29 +4,15 @@ import java.util.Scanner;
 
 public class MoneyCount {
     public static void main(String[] args) {
-        int money;
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("금액을 넣어주세요: ");
 
-        money = sc.nextInt();
 
-        int a = (money / 50000);
-        int b = (money % 50000) / 10000;
-        int c = (money % 10000) / 5000;
-        int d = (money % 5000) / 1000;
-        int f = (money % 1000) / 500;
-        int g = (money % 500) / 100;
-        int h = (money % 100) / 50;
-        int i = (money % 50) / 10;
+        int refund = 25000;
 
-        System.out.println("50000원 " + a + "장");
-        System.out.println("10000원 " + b + "장");
-        System.out.println("5000원 " + c + "장");
-        System.out.println("1000원 " + d + "장");
-        System.out.println("500원 " + f + "개");
-        System.out.println("100원 " + g + "개");
-        System.out.println("50원 " + h + "개");
-        System.out.println("10원 " + i + "개");
+        System.out.printf("5만원권 %d장 나머지:d\n",refund/50000,refund%50000);
+
+        System.out.printf("1만원권 %d장 나머지:d\n",refund/10000,refund%10000);
+
+        System.out.printf("5000원권 %d장 나머지:d\n",refund/10000,refund%5000);
     }
 }
