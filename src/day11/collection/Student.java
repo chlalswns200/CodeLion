1cc1aea1ce2703bfdede5ee3bd197c8a3142178d
chlalswns200 <chlalswns200@naver.com>
2022-10-05 16:10:47 +0900

    Student 이름 반, 깃허브 주소 추가

---
+++ src/day11/collection/Student.java
@@ -0,0 +1,26 @@
+package day11.collection;
+
+public class Student {
+
+    private String name;
+    private int classNumber;
+    private String gitRepository;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    public String getGitRepository() {
+        return gitRepository;
+    }
+
+    public Student(String name, int classNumber, String gitRepository) {
+        this.name = name;
+        this.classNumber = classNumber;
+        this.gitRepository = gitRepository;
+    }
+}
