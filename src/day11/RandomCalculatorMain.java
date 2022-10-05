310e2304bf6bd654946abc7aec5e5f32c8b3f154
chlalswns200 <chlalswns200@naver.com>
2022-10-05 11:43:22 +0900

    RandomCalculator 생성

--- src/day11/RandomCalculatorMain.java
+++ src/day11/RandomCalculatorMain.java
@@ -6,10 +6,10 @@ public class RandomCalculatorMain {
         RandomCalculator rc = new RandomCalculator(10);
         int b = (int)(Math.random()*10);
 
-        rc.plus(b);
-        rc.minus(b);
-        rc.multiple(b);
-        rc.divide(b);
+        rc.plus();
+        rc.minus();
+        rc.multiple();
+        rc.divide();
 
     }
 }
