8f45be59b999d2ead2638f2c50cac22606104bdc
chlalswns200 <chlalswns200@naver.com>
2022-10-06 15:11:55 +0900

    현재 디렉토리 파일목록

---
+++ src/week3_day3/file/FileExercise.java
@@ -0,0 +1,17 @@
+package week3_day3.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileExercise {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader fileReader = new FileReader("./a_file.txt");
+
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for (File file : files) {
+            System.out.println("file = " + file);
+        }
+    }
+}
