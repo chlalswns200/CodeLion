2f74dc89fff73444ce10d02a0905b592bb352b1d
chlalswns200 <chlalswns200@naver.com>
2022-10-06 13:28:36 +0900

    알파벳 체크 수정

--- src/week3_day3/collection/AlphabetCnt.java
+++ src/week3_day3/collection/AlphabetCnt.java
@@ -2,19 +2,26 @@ package week3_day3.collection;
 
 public class AlphabetCnt {
 
-    public static boolean isAlphabet(char a) {
+    public AlphabetCnt() {
+    }
+
+    public boolean isAlphabet(char a) {
+
         if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z')) {
             return true;
         } else {
             return false;
         }
+
     }
 
     public static void main(String[] args) {
         String s1 = "aabbcc!ddefghijkAAAZZ!Zkkkklm";
 
+        AlphabetCnt alpha = new AlphabetCnt();
+
         for (int i = 0; i < s1.length(); i++) {
-            if (isAlphabet(s1.charAt(i))) {
+            if (alpha.isAlphabet(s1.charAt(i))) {
                 System.out.println(s1.charAt(i)+"는 알파벳 입니다.");
             } else {
                 System.out.println(s1.charAt(i)+"는 알파벳이 아닙니다.");
