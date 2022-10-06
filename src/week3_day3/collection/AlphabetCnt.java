ccebba53e3cc7ca807259450f7a4aff4f58c8c59
chlalswns200 <chlalswns200@naver.com>
2022-10-06 13:44:25 +0900

    알파벳 체크 수업 중

--- src/week3_day3/collection/AlphabetCnt.java
+++ src/week3_day3/collection/AlphabetCnt.java
@@ -1,5 +1,7 @@
 package week3_day3.collection;
 
+import java.util.HashMap;
+
 public class AlphabetCnt {
 
     public AlphabetCnt() {
@@ -17,15 +19,21 @@ public class AlphabetCnt {
 
     public static void main(String[] args) {
         String s1 = "aabbcc!ddefghijkAAAZZ!Zkkkklm";
-
         AlphabetCnt alpha = new AlphabetCnt();
+        s1 = s1.toUpperCase();
+        
+        HashMap<Character,Integer> alphabetMap = new HashMap<>();
+
+        for (char c = 'A'; c <= 'Z'; c++) {
+            alphabetMap.put(c, 0);
+        }
 
         for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
             if (alpha.isAlphabet(s1.charAt(i))) {
-                System.out.println(s1.charAt(i)+"는 알파벳 입니다.");
-            } else {
-                System.out.println(s1.charAt(i)+"는 알파벳이 아닙니다.");
+                alphabetMap.put(c, alphabetMap.get(c) + 1);
             }
         }
+        System.out.println("alphabetMap = " + alphabetMap);
     }
 }
